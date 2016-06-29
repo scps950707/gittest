a219386b339991e0b955ab1b3c4c76bb3b2cf620
scps950707 <scps950707@gmail.com>
2016-06-29 18:53:47 +0800

    test

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
  * Author:         scps950707
  * Email:          scps950707@gmail.com
  * Created:        2016-06-29 17:21
- * Last Modified:  2016-06-29 18:51
+ * Last Modified:  2016-06-29 18:53
  * Filename:       test.cpp
  * Purpose:        asd
  */
@@ -14,5 +14,6 @@ using namespace std;
 int main( void )
 {
     cout << "hello" << endl;
+    asdasd
     return 0;
 }
