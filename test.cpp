4629e2c8bd3dd22e1ad80ab30c1d94997855503e
scps950707 <scps950707@gmail.com>
2016-06-29 18:51:29 +0800

    test TIMESTAMP hook

--- test.cpp
+++ test.cpp
@@ -1,3 +1,14 @@
+/*
+ * Author:         scps950707
+ * Email:          scps950707@gmail.com
+ * Created:        2016-06-29 17:21
+ * Last Modified:  2016-06-29 18:51
+ * Filename:       test.cpp
+ * Purpose:        asd
+ */
+
+
+
 #include<iostream>
 using namespace std;
 int main( void )
--- test2.cpp
+++ test2.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main( void )
 {
-
+    cout << "hello world"<< endl;
     return 0;
 }
