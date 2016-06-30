8346996d693070e25b6e47e61a1f33c55f7cafbd
scps950707 <scps950707@gmail.com>
2016-06-30 22:39:05 +0800

    test

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
  * Author:         scps950707
  * Email:          scps950707@gmail.com
  * Created:        2016-06-30 21:52
- * Last Modified:  2016-06-30 22:38
+ * Last Modified:  2016-06-30 22:39
  * Filename:       test.cpp
  */
 
@@ -10,5 +10,6 @@ int main( void )
 {
     cout << "this line is staged" << endl;
     cout << "current code here" << endl;
+    cout << "but this line not" << endl;
     return 0;
 }
