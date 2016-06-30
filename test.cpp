42ffdc539e00ad2696733275cd290f243eee646d
scps950707 <scps950707@gmail.com>
2016-06-30 22:36:47 +0800

    test

--- test.cpp
+++ test.cpp
@@ -2,7 +2,12 @@
  * Author:         scps950707
  * Email:          scps950707@gmail.com
  * Created:        2016-06-30 21:52
- * Last Modified:  2016-06-30 21:59
+ * Last Modified:  2016-06-30 22:36
  * Filename:       test.cpp
  */
 
+int main( void )
+{
+    cout << "current code here" << endl;
+    return 0;
+}
