90627011922ef44262f540d03834446f4e3ed01e
scps950707 <scps950707@gmail.com>
2016-06-30 13:42:16 +0800

    test

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
  * Author:         scps950707
  * Email:          scps950707@gmail.com
  * Created:        2016-06-29 17:21
- * Last Modified:  2016-06-30 04:42
+ * Last Modified:  2016-06-30 13:42
  * Filename:       test.cpp
  * Purpose:        asd
  */
@@ -13,6 +13,7 @@
 using namespace std;
 int main( void )
 {
+    cout << "hello" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
     cout << "hello" << endl;
