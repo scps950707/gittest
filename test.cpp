e95a7713957cbcef2d6118157a3983436b6f5c62
scps950707 <scps950707@gmail.com>
2016-06-30 23:30:54 +0800

    asdfasdf

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
  * Author:         scps950707
  * Email:          scps950707@gmail.com
  * Created:        2016-06-30 21:52
- * Last Modified:  2016-06-30 23:28
+ * Last Modified:  2016-06-30 23:30
  * Filename:       test.cpp
  */
 
@@ -11,7 +11,7 @@ using namespace std;
 
 int main( void )
 {
-    cout << "this line is staged" << endl;
+    cout << "1this line is staged" << endl;
     cout << "current code here" << endl;
     cout << "but this line not" << endl;
     cout << "this new line" << endl;
